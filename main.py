21c06e67706b3a059b2665dd46ba02102683f001
happy-little-fish <2896417317@qq.com>
2024-10-13 17:59:54 +0800

    Update main.py

--- main.py
+++ main.py
@@ -76,8 +76,7 @@ def get_Today_Class():
     month = localtime().tm_mon
     day = localtime().tm_mday
     today = datetime.date(datetime(year=year, month=month, day=day))
-    print（123）
-    #todayClasses = get_Week_Classes(today.weekday())
+    todayClasses = get_Week_Classes(today.weekday())
     return todayClasses
 
 
